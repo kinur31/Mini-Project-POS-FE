ed486f9f3ea7b4f8231904d28b5c9fa47b1a6ef9
Rizky Noor Ichwanudin <46215481+kinur31@users.noreply.github.com>
2023-11-10 15:16:01 +0700

    init (#1)

---
+++ src/pages/home/index.js
@@ -0,0 +1,15 @@
+import { Box, Text, Button } from "@chakra-ui/react"
+
+const Home = () => {
+    console.log("Testing")
+    return (
+        <Box>
+        <Text>Test</Text>
+        </Box>
+        // <>
+        // <p>halo test</p>
+        // </>
+    )
+}
+
+export default Home; 
