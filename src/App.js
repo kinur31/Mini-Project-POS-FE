6d2ef3d5b96c0feab51902673efafda823339540
Sando-Mutaz <143694001+Sando-Mutaz@users.noreply.github.com>
2023-11-16 22:34:37 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import LoginScreen from "./pages/login/loginScreen";
 import LoginAdmin from "./pages/login/loginAdmin";
-import ForgotPassword from "./pages/forgotPassword/forgotPassword1";
+import ForgotPassword from "./pages/forgotPassword/forgotPassword";
+import CekEmail from "./pages/forgotPassword/cekEmail";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<LoginScreen />} />
         <Route path="/login-admin" element={<LoginAdmin />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/cek-email" element={<CekEmail />} />
         <Route path="/dashboard-admin" element={<LoginAdmin />} />
       </Routes>
     </>
