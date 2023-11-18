0ce640f04354f5c3bc92ab5ff4edd252f6985ff9
Sando-Mutaz <143694001+Sando-Mutaz@users.noreply.github.com>
2023-11-18 22:06:22 +0700

    init

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import LoginCashier from "./pages/login/loginCashier";
 import ForgotPassword from "./pages/forgotPassword/forgotPassword";
 import CekEmail from "./pages/forgotPassword/cekEmail";
 import ResetPassword from "./pages/resetPassword/resetPassword";
+import Auth from "./components/auth";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<LoginScreen />} />
-        <Route path="/login-cashier" element={<LoginCashier />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/auth/reset-password" element={<ResetPassword />} />
-        <Route path="/cek-email" element={<CekEmail />} />
-      </Routes>
+      <Auth>
+        <Routes>
+          <Route path="/" element={<LoginScreen />} />
+          <Route path="/login-cashier" element={<LoginCashier />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/auth/reset-password" element={<ResetPassword />} />
+          <Route path="/cek-email" element={<CekEmail />} />
+        </Routes>
+      </Auth>
     </>
   );
 }
