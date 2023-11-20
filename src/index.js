46939e7d0e9c46cd26760d6eb81b97c16b05c9a2
Rizky Noor Ichwanudin <rizky.noor.ichwanudin@gmail.com>
2023-11-20 21:39:19 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,8 +10,6 @@ import { Provider } from "react-redux";
 import { store } from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-import store from "./redux/store";
 
 ReactDOM.render(
   <Provider store={store}>
