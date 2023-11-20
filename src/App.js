df9beb3d30f0b27ce4e81ab0219c371aee658a36
Sando-Mutaz <143694001+Sando-Mutaz@users.noreply.github.com>
2023-11-20 15:57:36 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import DashboardAdmin from "./pages/dashboard/dashboardAdmin";
 import Profile from "./components/profile/profile";
+import Auth from "./components/auth";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/user-management" element={<DashboardAdmin />} />
-        <Route path="/test" element={<Profile />} />
-      </Routes>
+      <Auth>
+        <Routes>
+          <Route path="/user-management" element={<DashboardAdmin />} />
+          <Route path="/test" element={<Profile />} />
+        </Routes>
+      </Auth>
     </>
   );
 }
