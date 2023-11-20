d1aa6a8eae920c99ae97423059671549585ac042
Sando-Mutaz <143694001+Sando-Mutaz@users.noreply.github.com>
2023-11-20 13:09:28 +0700

    init

---
+++ src/components/modalEditCashier/modalEditCashier.jsx
@@ -0,0 +1,11 @@
+import { Box, Text } from "@chakra-ui/react";
+
+const ModalEditCashier = () => {
+  return (
+    <Box>
+      <Text>test</Text>
+    </Box>
+  );
+};
+
+export default ModalEditCashier;
