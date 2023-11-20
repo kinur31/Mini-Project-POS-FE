05bdcdff961fe2b5249f5cce069db0dd05acb873
Sando-Mutaz <143694001+Sando-Mutaz@users.noreply.github.com>
2023-11-20 22:17:31 +0700

    init

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ function App() {
     <>
       <Auth>
         <Routes>
+          <Route path="/" element={<LoginScreen />} />
+          <Route path="/login-admin" element={<LoginAdmin />} />
+          <Route path="/login-cashier" element={<LoginCashier />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/auth/reset-password" element={<ResetPassword />} />
+          <Route path="/cek-email" element={<CekEmail />} />
           <Route path="/user-management" element={<DashboardAdmin />} />
         </Routes>
       </Auth>
